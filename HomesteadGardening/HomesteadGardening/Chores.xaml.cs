4a4787535e2be099dc76730c54fedfb8e44ea9ba
Josh Baisc <joshbawesome@hotmail.com>
2019-09-23 15:53:48 -0700

    Initial Commit

---
+++ HomesteadGardening/HomesteadGardening/Chores.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomesteadGardening
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Chores : ContentPage
+	{
+		public Chores ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
