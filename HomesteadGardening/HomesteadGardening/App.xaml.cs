0a572a4748a56df36bbea1b6a8a754f37064db76
Josh Baisc <joshbawesome@hotmail.com>
2019-09-23 15:36:57 -0700

    Add project files.

---
+++ HomesteadGardening/HomesteadGardening/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HomesteadGardening.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace HomesteadGardening {
+    public partial class App : Application {
+
+        public App() {
+            InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
